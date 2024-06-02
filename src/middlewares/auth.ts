d5ce49aa9ff0d0559b1e2e9dc0fed4e3f8d17838
safoan <safibazine@gmail.com>
2024-06-02 19:50:38 +0000

    fixed some errors

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,6 +2,9 @@ import { NextFunction , Request , Response } from "express";
 import { UnauthrizedException } from "../exceptions/unauthorized";
 import { ErrorCode } from "../exceptions/root";
 import * as jwt from 'jsonwebtoken';
+import { JWT_SECRET } from "../secrets";
+import { prismaClient } from "..";
+import { User } from "@prisma/client";
 
 
 const authMiddleware = async (req : Request , res : Response , next :NextFunction) => {
@@ -13,9 +16,23 @@ const token = req.headers.authrization
 if (!token) {
     next( new UnauthrizedException ('Unauthrized' , ErrorCode.UNAUTHORIZED_EXCEPTION))
 } 
+try {
+
 // 3. if token is present , verify that token & extract tha payload 
+const payload = jwt.verify(token as string , JWT_SECRET) as any
 // 4. to get the user from the payload 
+const user = await prismaClient.user.findFirst ({where : {id:payload.userId}})
+if (!user) {
+    next( new UnauthrizedException ('Unauthrized' , ErrorCode.UNAUTHORIZED_EXCEPTION))
+}
 // 5. to attach the user to the current request object 
+req.user = user as User
+next()
+}
+catch (error) {
+    next( new UnauthrizedException ('Unauthrized' , ErrorCode.UNAUTHORIZED_EXCEPTION))
+}
+
 
 
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,15 @@
 import {Router} from 'express'
-import { signup } from '../controllers/auth'
+import { signup , me} from '../controllers/auth'
 import { login } from '../controllers/auth'
 import { erorHandler } from '../error-handler'
 import authMiddleware from '../middlewares/auth'
 
-const authRoutes:Router = Router()
 
-authRoutes.post('/signup', erorHandler (signup))
-authRoutes.post('/login' , erorHandler(login))
-authRoutes.get('/me' , [authMiddleware],erorHandler(me))
 
-export  default authRoutes
+const authRoutes: Router = Router();
+
+authRoutes.post('/signup', erorHandler(signup));
+authRoutes.post('/login', erorHandler(login));
+authRoutes.get('/me', [authMiddleware], erorHandler(me));
+
+export default authRoutes;
